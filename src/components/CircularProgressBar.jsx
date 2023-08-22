5a9f0706e20db65797284993328e8a802f0fd29f
miguel <miguel.gutierrez.jariod@estudiantat.upc.edu>
2023-08-22 19:52:47 +0200

    dev: added circular progress bar for languages

---
+++ src/components/CircularProgressBar.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import {buildStyles, CircularProgressbar, CircularProgressbarWithChildren} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+const CircularProgressBar = ({value, src, color}) => {
+    /*const [percentage, setPercentage] = useState(0);
+    useEffect(() => {
+        setTimeout(() => {
+            if (percentage < value) {
+                setPercentage(percentage + 1);
+            }
+        }, 50);
+    }, [percentage]);*/
+
+    return (
+        <div style={{textAlign:"center"}}>
+            <div style={{ width: 150}}>
+                <CircularProgressbarWithChildren value={value} styles={buildStyles({
+                    pathTransitionDuration: 1,
+                    pathColor: color,
+                    textColor: color,
+                    trailColor: '#d6d6d6',
+                })} >
+                    <img
+                        style={{ width: 80 }}
+                        src={src}
+                        alt=""
+                    />
+                </CircularProgressbarWithChildren>
+            </div>
+        </div>
+    );
+}
+export default CircularProgressBar;
+
+/*
+<div style={{textAlign:"center"}}>
+            <div style={{ width: 150}}>
+                <CircularProgressbarWithChildren value={percentage} styles={buildStyles({
+                    pathTransitionDuration: 1,
+                    pathColor: '#a8379f',
+                    textColor: '#a8379f',
+                    trailColor: '#d6d6d6',
+                })} >
+                <img
+                    style={{ width: 80, marginTop: -5 }}
+                    src="https://i.imgur.com/b9NyUGm.png"
+                    alt="doge"
+                />
+                </CircularProgressbarWithChildren>
+            </div>
+        </div>
+*/
